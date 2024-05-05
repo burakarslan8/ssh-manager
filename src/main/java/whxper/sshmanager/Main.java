5a9a62fcd868d5ccf23069af404df1cce5267512
burakarslan8 <burakarslan271@gmail.com>
2024-05-05 16:48:51 +0300

    listview added

--- src/main/java/whxper/sshmanager/Main.java
+++ src/main/java/whxper/sshmanager/Main.java
@@ -1,11 +1,14 @@
 package whxper.sshmanager;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +17,9 @@ public class Main extends Application {
 
     @FXML
     private Button newHostButton;
+    private ListView<String> hostList;
+    private ObservableList<String> hosts;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
@@ -22,7 +28,15 @@ public class Main extends Application {
 
         controller.newHostButton.setOnAction(event -> {
             System.out.println("Button clicked");
+            controller.hosts.add("New Host");
         });
+
+        controller.hosts = FXCollections.observableArrayList();
+        controller.hosts.addAll("Host1", "Host2", "Host3");
+
+        controller.hostList = (ListView<String>) root.lookup("#hostList");
+        controller.hostList.setItems(controller.hosts);
+
 
         Scene scene = new Scene(root, 800, 600);
         stage.setTitle("SSH Manager");
